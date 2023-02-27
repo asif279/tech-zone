df7a9156087ca78a28f6bf6ce91ee849731d1678
asif279 <asifofficial8130@gmail.com>
2023-02-27 13:13:58 +0600

    auth

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './SignUp.css'
 
 const SignUp = () => {
+    const [erorr,setError]=useState(null);
+
+  const handleSubmit =(event)=>{
+    event.preventDefault();
+    const form= event.target;
+    const email =form.email.value;
+    const password=form.password.value;
+    const confirm= form.confirm.value;
+    console.log(email,password,confirm);
+    if(password.length<6){
+        setError("Password must be 6 character long");
+        return;
+    }
+
+    if(password!==confirm){
+        setError("Your Password Didn't match ");
+        return;
+    }
+
+  }
+
+
     return (
         <div className='form-container'>
         <h2 className='form-title'>SignUp</h2>
-        <form action="">
+        <form action=""onSubmit={handleSubmit}>
 <div className="form-control">
     <label htmlFor="email">Email</label>
     <input type="email" name="email" id="" required placeholder='Enter Your Email' />
@@ -21,10 +43,11 @@ const SignUp = () => {
     <label htmlFor="confirm"> Confirm Password</label>
     <input type="password" name="confirm" id="" required placeholder='Re-Enter Your Password' />
 </div>
-<input className='btn-submit' type="submit" value="Login" />
+<input className='btn-submit' type="submit" value="Sign Up" />
 
         </form>
         <p>Already Have Account? <Link to="/login">Login</Link></p>
+        <p className='text-error'>{erorr}</p>
             
         </div>
     );
--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,10 +1,18 @@
 import React, { createContext } from 'react';
+import {getAuth} from 'firebase/auth';
+import app from '../Firebase/firebase.config';
 
-const AuthContext = createContext()
+export const AuthContext = createContext();
+const auth =getAuth(app);
 
-const UserContext = () => {
+
+const user={email:'abc'};
+const AuthInfo={user};
+
+const UserContext = ({children}) => {
     return (
-        <AuthContext.Provider>
+        <AuthContext.Provider value={AuthInfo}>
+        {children}
             
         </AuthContext.Provider>
     );
